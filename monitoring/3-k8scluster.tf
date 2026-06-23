42942080a228f3c0a17f604ac4f3a1f422bce11f
Chahat Yadav <160589578+Chahatyadav1@users.noreply.github.com>
2026-06-23 15:13:12 +0530

    Update 3-k8scluster.tf

--- monitoring/3-k8scluster.tf
+++ monitoring/3-k8scluster.tf
@@ -21,7 +21,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = var.service_account
 
     oauth_scopes = [
